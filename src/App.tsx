e34d624a82eaecd1248240b02f68b75da3c6f8fa
zk524 <20262010007@fudan.edu.cn>
2023-02-21 12:48:29 +0800

    modify

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import { ReactNode, ChangeEvent, useEffect, useRef, useState, useMemo } from "react";
-import { Dialog, DialogContent, DialogTitle, Link, Input, IconButton } from "@mui/material";
+import { ChangeEvent, useRef, useState, useMemo } from "react";
+import { Dialog, DialogContent, DialogTitle, Input, IconButton } from "@mui/material";
 import { QrCodeScanner } from '@mui/icons-material';
 import { BrowserQRCodeReader, IScannerControls } from '@zxing/browser';
 import { toDataURL } from 'qrcode';
-
-const Frame = (props: { open?: boolean, children?: ReactNode, buttons?: ReactNode[] }) => {
-  return <Dialog open={props.open ?? true} fullWidth>
-    <DialogTitle display='flex'>
-      <Link href='/' flexGrow={1}>QRcode</Link>
-      {props.buttons?.map((b: ReactNode) => b)}
-    </DialogTitle>
-    <DialogContent>{props.children}</DialogContent>
-  </Dialog>;
-}
 
 export default () => {
   const ctrl: { scan?: Promise<IScannerControls> } = {}
@@ -21,18 +11,20 @@ export default () => {
   const [qr, QR] = useState(['']);
   const codeReader = useMemo(() => new BrowserQRCodeReader(new Map().set(2, [11]), { delayBetweenScanAttempts: 100, delayBetweenScanSuccess: 100 }), []);
   const toggleScan = () => {
-    if (scan) (SCAN(false), navigator.mediaDevices.getUserMedia().then((m) => m.getTracks()[1].stop()))
+    if (scan) (console.log(ctrl.scan), SCAN(false))
     else (SCAN(true), ctrl.scan = codeReader.decodeFromVideoDevice(void 0, 'video', handleScan))
   }
   const handleScan = (s: any) => {
     const data = s?.getText() ?? ''
-    if (data) {
-      ctrl.scan!.then((s) => (s.stop(), SCAN(false)))
-      inputRef.current!.value = data
-    }
+    data && ctrl.scan!.then((s) => (s.stop(), SCAN(false), QR(['']), inputRef.current!.value = data))
   }
-  return <Frame buttons={[<IconButton key='scanner' onClick={toggleScan} children={<QrCodeScanner color='primary' />} />]}>
-    <Input fullWidth inputRef={inputRef} placeholder={'CONTEXT'} onChange={(e: ChangeEvent<HTMLInputElement>) => toDataURL(e.target!.value).then((data) => { QR([data]) })} />
-    {scan ? <video id="video" style={{ width: '100%' }} /> : qr.map((src, i) => <img key={src + i} style={{ width: '100%' }} src={src} />)}
-  </Frame>;
+  return <Dialog open fullWidth>
+    <DialogTitle display='flex'>
+      <IconButton key='scanner' onClick={toggleScan} children={<QrCodeScanner color='primary' />} />
+    </DialogTitle>
+    <DialogContent>
+      <Input fullWidth inputRef={inputRef} placeholder={'CONTEXT'} onChange={(e: ChangeEvent<HTMLInputElement>) => toDataURL(e.target!.value, { margin: 1, scale: 16 }).then((data) => { QR([data]) }).catch(() => { })} />
+      {scan ? <video id="video" style={{ width: '100%' }} /> : qr.map((src, i) => <img key={src + i} style={{ width: '100%' }} src={src} />)}
+    </DialogContent>
+  </Dialog>
 }
