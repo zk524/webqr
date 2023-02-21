d58db44dccd7db020e08fc2127b018c5f3c16d63
zk524 <20262010007@fudan.edu.cn>
2023-02-21 13:36:26 +0800

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,16 @@ export default () => {
   const codeReader = useMemo(() => new BrowserQRCodeReader(new Map().set(2, [11]), { delayBetweenScanAttempts: 100, delayBetweenScanSuccess: 100 }), []);
   const toggleScan = () => {
     if (scan) { BrowserQRCodeReader.releaseAllStreams(); SCAN(false) }
-    else SCAN(true); ctrl.scan = codeReader.decodeFromVideoDevice(void 0, 'video', (data: any) => data && ctrl.scan!.then((s) => (s.stop(), SCAN(false), QR(['']), inputRef.current!.value = data!.getText())))
+    else { SCAN(true); ctrl.scan = codeReader.decodeFromVideoDevice(void 0, 'video', (data: any) => data && ctrl.scan!.then((s) => (s.stop(), SCAN(false), QR(['']), inputRef.current!.value = data!.getText()))) }
   }
   return <Dialog open fullWidth>
     <DialogTitle display='flex'>
       <IconButton key='scanner' onClick={toggleScan} children={<QrCodeScanner color='primary' />} />
     </DialogTitle>
     <DialogContent>
-      <Input fullWidth inputRef={inputRef} placeholder={'CONTEXT'} onChange={(e: ChangeEvent<HTMLInputElement>) => toDataURL(e.target!.value, { margin: 1, scale: 16 }).then((data) => { QR([data]) }).catch(() => { })} />
+      <Input fullWidth inputRef={inputRef} placeholder={'CONTEXT'} onChange={(e: ChangeEvent<HTMLInputElement>) => toDataURL(e.target!.value, { margin: 1, scale: 16 }).then((data) => {
+        QR([data])
+      }).catch(() => { })} />
       {scan ? <video id="video" style={{ width: '100%' }} /> : qr.map((src, i) => <img key={src + i} style={{ width: '100%' }} src={src} />)}
     </DialogContent>
   </Dialog>
